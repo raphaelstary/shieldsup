9f609707b326d3151754bb54447810644e8033a3
Raphael Stary <raphael.stary@gmail.com>
2014-11-20 23:23:52 +0100

    core stuff + reformatting + button factory

--- www/js/scene/position/changeSign.js
+++ www/js/scene/position/changeSign.js
@@ -3,7 +3,7 @@ var changeSign = (function () {
 
     function changeSign(fn) {
         return function (arg1, arg2) {
-            return - fn(arg1, arg2);
+            return -fn(arg1, arg2);
         }
     }
 
