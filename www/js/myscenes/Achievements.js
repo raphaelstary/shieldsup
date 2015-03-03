f599e9a8017a609dcec9353a357cd9bf43b78f84
raphaelstary <raphael.stary@gmail.com>
2015-03-03 10:34:12 +0100

    achievements without functionality refs #69 #game

--- www/js/myscenes/Achievements.js
+++ www/js/myscenes/Achievements.js
@@ -1,4 +1,4 @@
-var Achievements = (function () {
+var Achievements = (function (Width, Height, Font, subtract, add, Math) {
     "use strict";
 
     function Achievements(services) {
@@ -9,6 +9,14 @@ var Achievements = (function () {
     }
 
     var KEY = 'achievements';
+    var DONE = 'done';
+    var CONQUEROR = 'conqueror';
+    var SHOPPING_QUEEN = 'shopping_queen';
+    var CLOSER = 'closer';
+    var LIVE_ONCE = 'live_once';
+    var STAR_GAZER = 'star_gazer';
+    var FASTER_THAN_LIGHT = 'faster_than_light';
+
     var COMMON_KEY = 'common_buttons';
     var BACK = 'back';
 
@@ -17,6 +25,7 @@ var Achievements = (function () {
     var WHITE = '#fff';
     var VIOLET = '#3a2e3f';
     var GOLD = '#ffd700';
+    var DARK_GOLD = '#B8860B';
     var LIGHT_GREY = '#c4c4c4';
 
     Achievements.prototype.show = function (next) {
@@ -27,22 +36,24 @@ var Achievements = (function () {
             self.messages.get(COMMON_KEY, BACK), endScene, 7);
         self.sceneStorage.menuSceneButtons.push(backButton);
 
-        drawAchievement(Height.get(48, 8), 'Conqueror', 'Defeat the Monster at the End of the Asteroid');
-        drawAchievement(Height.get(48, 14), 'Shopping Queen', 'Buy everything in the Shop');
-        drawAchievement(Height.get(48, 20), 'The Finisher', 'Complete all Quests', true);
-        drawAchievement(Height.get(48, 26), 'You Only Live Once', 'Loose no Life in a complete run.');
-        drawAchievement(Height.get(48, 32), 'Star Gazer', 'Collect all Stars in a complete run');
-        drawAchievement(Height.get(48, 38), 'Faster than Light', 'Complete the Quest mode in 1 hour or less');
-        drawAchievement(Height.get(48, 44), 'Asteroid Miner', 'Destroy every Asteroid in a complete run');
+        drawAchievement(Height.get(48, 8), CONQUEROR);
+        drawAchievement(Height.get(48, 14), SHOPPING_QUEEN);
+        drawAchievement(Height.get(48, 20), CLOSER, true);
+        drawAchievement(Height.get(48, 26), LIVE_ONCE);
+        drawAchievement(Height.get(48, 32), STAR_GAZER);
+        drawAchievement(Height.get(48, 38), FASTER_THAN_LIGHT);
 
-
-        function drawAchievement(yFn, header, description, complete) {
+        function drawAchievement(yFn, header, complete) {
             var achievement_1_bg = self.stage.drawRectangle(Width.HALF, yFn, Width.get(10, 9), Height.get(480, 50),
-                complete ? GOLD : VIOLET, true, undefined, 7);
-            var achievement_1_header = self.stage.drawText(Width.HALF, subtract(yFn, Height.get(480, 13)), header,
-                Font._40, SPECIAL_FONT, WHITE, 8);
-            var achievement_1_text = self.stage.drawText(Width.HALF, add(yFn, Height.get(480, 10)), description,
-                Font._60, FONT, LIGHT_GREY, 8);
+                complete ? DARK_GOLD : VIOLET, true, undefined, 7);
+            if (complete) {
+                drawables.push(self.stage.drawText(Width.get(20, 17), subtract(yFn, Height.get(480, 10)),
+                    self.messages.get(KEY, DONE), Font._40, SPECIAL_FONT, GOLD, 8, undefined, Math.PI / 8));
+            }
+            var achievement_1_header = self.stage.drawText(Width.HALF, subtract(yFn, Height.get(480, 13)),
+                self.messages.get(KEY, header), Font._40, SPECIAL_FONT, WHITE, 8);
+            var achievement_1_text = self.stage.drawText(Width.HALF, add(yFn, Height.get(480, 10)),
+                self.messages.get(KEY, header + '_description'), Font._60, FONT, LIGHT_GREY, 8);
             drawables.push(achievement_1_bg, achievement_1_header, achievement_1_text);
         }
 
@@ -63,4 +74,4 @@ var Achievements = (function () {
     };
 
     return Achievements;
-})();
+})(Width, Height, Font, subtract, add, Math);
